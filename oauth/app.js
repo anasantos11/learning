0f5065ea69b74ee5894bef6ef4bc1b035234a78f
Ana <anasantos1103@gmail.com>
2018-09-07 10:59:32 -0300

    Incluso método para solicitar token por jquery

--- oauth/app.js
+++ oauth/app.js
@@ -1,3 +1,25 @@
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
+var token = '';
+var clienteId = '2030589643652951'; // Application ID - Substitua pelo seu APP ID
+var url = "https://app-apds.azurewebsites.net/"; // URL da sua página - Substitua pela sua
+
+function getToken() {
+  $.ajax({
+    url: "https://graph.facebook.com/v3.1/oauth/access_token",
+    data: {
+      "client_id": clienteId,
+      "redirect_uri": url
+    },
+    type: "GET",
+    success: function (response) {
+      var resposta = response;
+      debugger;
+    },
+    error: function (error) {
+      var erro = error;
+      debugger;
+    }
+  })
+}
+
+getToken();
+
