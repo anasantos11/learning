36295516029793d46e9344b02bb39f94372d055b
Ana <anasantos1103@gmail.com>
2018-09-07 19:10:38 -0300

    .

--- oauth/app.js
+++ oauth/app.js
@@ -1,26 +1,51 @@
 var token = '';
+var code = '';
 var clienteId = '2030589643652951'; // Application ID - Substitua pelo seu APP ID
-var url = "https://app-apds.azurewebsites.net/oauth"; // URL da sua página - Substitua pela sua
+var url = "http://localhost:8080/"; // URL da sua página - Substitua pela sua
 
-function getToken() {
+
+function getSpotifyVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) { return pair[1]; }
+  }
+  return (false);
+}
+
+
+function getCode() {
   $.ajax({
     url: "https://www.facebook.com/v3.1/dialog/oauth?",
     data: {
       "client_id": clienteId,
-      "redirect_uri": url,
-      "state": "st=state123abc,ds=123456789"
+      "redirect_uri": url
     },
     type: "GET",
+    crossDomain: true,
     success: function (response) {
       var resposta = response;
       debugger;
+      return response;
     },
     error: function (error) {
       var erro = error;
       debugger;
+      return null;
     }
   })
 }
 
-getToken();
+token = getSpotifyVariable('access_token');
+if (!token) {
+  code = getSpotifyVariable('code');
+  if (!code) {
+    window.location = "https://graph.facebook.com/oauth/authorize?client_id=" + clienteId + "&redirect_uri=" + url;
+  } else {
+    debugger;
+    window.location = "https://graph.facebook.com/v3.1/oauth/access_token?client_id=" + clienteId + "&redirect_uri=" + url + "&code=" + code + "&client_secret=" + "027551e060534e25e5ad5e137e36649f"
+  }
+}
+
 
